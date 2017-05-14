211bb470c841976c8c97601c5019d448d817b415
Honohonoho <7517188620@qq.com>
2017-05-14 21:47:24 +0800

    新增 todo

--- src/App.js
+++ src/App.js
@@ -9,28 +9,26 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'text',
-      todoList: [
-        {id: 1, title: '第一个待办'},
-        {id: 2, title: '第二个待办'},
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
 
   render() {
     let willdo = this.state.todoList.map((item,index)=>{
       return ( //多个节点时需要加括号,<li>的数量取决于todoList
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
     })
+    console.log(willdo)
 
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputArea">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {willdo}
@@ -38,9 +36,24 @@ class App extends Component {
       </div>
     )
   }
-  addTodo(){
-    console.log('我得添加一个 todo 了')
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMarker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0;
+function idMarker() {
+  id += 1;
+  return id;
+}
