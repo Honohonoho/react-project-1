4ab665717bf5c696dc0d7967394a24aabf0e808c
Honohonoho <751718620@qq.com>
2017-05-22 17:33:57 +0800

    添加登录/注册对话框-UserDialog

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './reset.css';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
+import UserDialog from './UserDialog';
 
 
 class App extends Component {
@@ -36,6 +37,7 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
+          <UserDialog />
       </div>
     )
   }
