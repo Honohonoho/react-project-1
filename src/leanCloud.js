9ac79c1eb54d11f431d8d4367bc2c05dcf9b92a7
Honohonoho <751718620@qq.com>
2017-05-26 16:59:45 +0800

    点击注册按钮后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,10 +1,34 @@
-import AV from 'leanclound-storage';
+import AV from 'leancloud-storage';
 
 var APP_ID = 'hQmOfHwO8BAIOOY48enYB85H-gzGzoHsz';
 var APP_KEY = 'UsUmRCkVuSQIptxpRJ99wlgh';
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+	appId: APP_ID,
+	appKey: APP_KEY
 });
 
 export default AV
+export function signUp(username,password,successFn,errorFn){
+	//新建 AVuser 对象实例
+	var user = new AV.User()
+	//设置用户名
+	user.setUsername(username)
+	//设置密码
+	user.setPassword(password)
+	//设置邮箱
+	//user.setEmail();
+	user.signUp().then(function(loginedUser){
+		console.log(loginedUser);
+		let user = getUserFormAVUser(loginedUser)
+		successFn.call(null,user)
+	},function(error){
+		errorFn.call(null,error)
+	})
+	return undefined
+}
+function getUserFormAVUser(AVUser){
+	return{
+		id: AVUser.id,
+		...AVUser.attributes
+	}
+}
