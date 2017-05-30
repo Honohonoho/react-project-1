ac667feee0d9c0250588ea4d79a4d12985ce8e98
Honohonoho <751718620@qq.com>
2017-05-30 14:25:59 +0800

    如果输入为空 则什么都不做

--- src/todoInput.js
+++ src/todoInput.js
@@ -3,7 +3,9 @@ import './TodoInput.css'
 
 function submit(props,e){
 	if(e.key === 'Enter'){
-		props.onSubmit(e)
+        if(e.target.value.trim() !== ''){
+            props.onSubmit(e)
+        }
 	}
 }
 
