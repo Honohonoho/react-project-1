e3b6ba474f6ccbc875a009cee8e3b9a6600a69aa
Honohonoho <751718620@qq.com>
2017-05-30 11:17:32 +0800

    将 TodoInput 改写成一个函数

--- src/todoInput.js
+++ src/todoInput.js
@@ -1,19 +1,19 @@
 import React,{ Component } from 'react';
 import './TodoInput.css'
 
-export default class TodoInput extends Component {
-	render(){
-		return <input type="text" placeholder="请输入待办事项" className="TodoInput" value={this.props.content}
-			onChange={this.changeTitle.bind(this)}
-			onKeyPress={this.submit.bind(this)} />
+function submit(props,e){
+	if(e.key === 'Enter'){
+		props.onSubmit(e)
 	}
-	submit(e){
-		if(e.key === 'Enter'){
-			// console.log('用户按回车了')
-			this.props.onSubmit(e)
-		}
-	}
-	changeTitle(e){
-		this.props.onChange(e)
-	}
+}
+
+function changeTitle(props,e){
+	props.onChange(e)
+}
+
+export default function (props){
+		return <input type="text" placeholder="请输入待办事项" className="TodoInput" 
+			value={props.content}
+			onChange={changeTitle.bind(null,props)} //bind()的第二个参数跟在this（或其他对象）后面，被插入到目标函数的参数列表的开始位置
+			onKeyPress={submit.bind(null,props)} />
 }
