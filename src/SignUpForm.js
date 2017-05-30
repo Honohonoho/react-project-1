968217cef73c7b8706e689de8b1417609b37fd1d
Honohonoho <751718620@qq.com>
2017-05-30 10:00:06 +0800

    将 SignUpForm 改成一个函数

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,31 +1,27 @@
-import React,{Component} from 'react';
-
-export default class SignUpForm extends Component{
-    render(){
-        return (
-            <form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/*注册*/}
-				<div className="row">
-					<label>邮箱</label>
-					<input type="text" autoFocus="autofocus" placeholder="请输入邮箱"
-						value={this.props.formData.email}
-						onChange={this.props.onChange.bind(this,'email')} /> {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
-				</div>
-				<div className="row">
-					<label>用户名</label>
-					<input type="text" placeholder="请输入用户名"
-						value={this.props.formData.username}
-						onChange={this.props.onChange.bind(this,'username')} /> {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
-				</div>
-				<div className="row">
-					<label>密码</label>
-					<input type="password" placeholder="请输入密码"
-						value={this.props.formData.password}
-						onChange={this.props.onChange.bind(this,'password')} />
-				</div>
-				<div className="row actions">
-					<button type="submit">注册</button>
-				</div>
-			</form>
-        )
-    }
+export default function (props){
+	return (
+		<form className="signUp" onSubmit={props.onSubmit.bind(this)}> {/*注册*/}
+			<div className="row">
+				<label>邮箱</label>
+				<input type="text" autoFocus="autofocus" placeholder="请输入邮箱"
+					value={props.formData.email}
+					onChange={props.onChange.bind(null,'email')} /> {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+			</div>
+			<div className="row">
+				<label>用户名</label>
+				<input type="text" placeholder="请输入用户名"
+					value={props.formData.username}
+					onChange={props.onChange.bind(null,'username')} /> {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
+			</div>
+			<div className="row">
+				<label>密码</label>
+				<input type="password" placeholder="请输入密码"
+					value={props.formData.password}
+					onChange={props.onChange.bind(null,'password')} />
+			</div>
+			<div className="row actions">
+				<button type="submit">注册</button>
+			</div>
+		</form>
+	)
 }
